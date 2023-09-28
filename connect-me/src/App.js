703e840090f4edcac75c138de464dcea5dd8dd23
Amartya Sen <amartyasen895@gmail.com>
2023-09-29 01:55:19 +0530

    added client

---
+++ connect-me/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-slate-300 w-28 h-28 flex justify-center items-center">
+      <h1>Hi I m Tailwind</h1>
+    </div>
+  );
+}
+
+export default App;
