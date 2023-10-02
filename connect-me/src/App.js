f45e29458ea4be6b1ed89aa4273bc3435cfdcfce
Amartya Sen <amartyasen895@gmail.com>
2023-10-02 16:13:35 +0530

    theme

--- connect-me/src/App.js
+++ connect-me/src/App.js
@@ -1,36 +1,48 @@
-import './App.css';
-import Navbar from './components/Navbar';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { Outlet, createBrowserRouter } from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import LoginScreen from "./screens/LoginScreen";
+import ProfileScreen from "./screens/ProfieScreen";
 
-import {Outlet, createBrowserRouter} from "react-router-dom"
-import HomeScreen from './screens/HomeScreen';
-import LoginScreen from './screens/LoginScreen';
-import ProfileScreen from './screens/ProfieScreen';
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./state/theme.js";
 
 function App() {
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
   return (
     <div>
-     <Navbar/>
-     <Outlet/>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Navbar />
+        <Outlet />
+      </ThemeProvider>
     </div>
   );
 }
 
 export const appRouter = createBrowserRouter([
   {
-    path:"/",
-    element:<App/>,
-    children:[
+    path: "/",
+    element: <App />,
+    children: [
       {
-        path:"/home",
-        element:<HomeScreen/>
-      },{
-        path:"/login",
-        element: <LoginScreen/>
+        path: "/home",
+        element: <HomeScreen />,
       },
       {
-        path:"/profile/:userId",
-        element: <ProfileScreen/>
-      }
-    ]
-  }
+        path: "/login",
+        element: <LoginScreen />,
+      },
+      {
+        path: "/profile/:userId",
+        element: <ProfileScreen />,
+      },
+    ],
+  },
 ]);
--- connect-me/src/state/store.js
+++ connect-me/src/state/store.js
@@ -12,18 +12,19 @@ import {
 
 import storage from "redux-persist/lib/storage";
 
-const persistConfig = {key : "root", storage , version : 1 };
+const persistConfig = { key: "root", storage, version: 1 };
 const persistedReducer = persistReducer(persistConfig, authSlice);
 
 const store = configureStore({
   reducer: persistedReducer,
-  middleware:(getDefaultMiddleware)=>{
-    getDefaultMiddleware({
-        serializableCheck:{
-            ignoredActions:[FLUSH,REHYDRATE,PAUSE,PERSIST,PURGE,REGISTER],
-        },
-    })
-  }
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    });
+  },
 });
+
 
 export default store;
